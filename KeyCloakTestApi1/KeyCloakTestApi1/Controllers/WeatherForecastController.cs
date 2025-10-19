6b63ed8e225d768315b6d4f9ea32e5f4273c0f4a
Okan Öz <okan.oz@outlook.com>
2025-10-19 20:57:46 +0300

    Added Postman Collection, Keycloak Realm Setting Json.

--- KeyCloakTestApi1/KeyCloakTestApi1/Controllers/WeatherForecastController.cs
+++ KeyCloakTestApi1/KeyCloakTestApi1/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KeyCloakTestApi1.Controllers;
 
@@ -18,6 +19,7 @@ public class WeatherForecastController : ControllerBase
         _logger = logger;
     }
 
+    [Authorize]
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
--- KeyCloakTestApi1/KeyCloakTestApi1/Program.cs
+++ KeyCloakTestApi1/KeyCloakTestApi1/Program.cs
@@ -1,7 +1,33 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    var authSection = builder.Configuration.GetSection("Authentication");
+
+    options.Authority = authSection["Authority"];
+    options.Audience = authSection["Audience"];
+    options.RequireHttpsMetadata = bool.Parse(authSection["RequireHttpsMetadata"] ?? "false");
+
+    // Keycloak JWKS endpoint'inden public key doğrulaması
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+
+builder.Services.AddAuthorization();
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -10,6 +36,7 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -18,8 +45,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
-
+app.UseAuthentication();  // Token doğrulama middleware
+app.UseAuthorization();   // Yetkilendirme middleware
 app.MapControllers();
 
 app.Run();
